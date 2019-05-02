9473f21423daf985a75294f1e938de604a3237d0
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-05-01 23:17:05 -0700

    shell done

---
+++ get_clicky/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+      <>
+        <h1>This is the Heading!</h1>
+      </>
+
+  );
+}
+
+export default App;
