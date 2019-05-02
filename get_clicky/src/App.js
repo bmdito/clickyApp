faa81900ae038e004a40d61dcc4b282491e8a93a
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-05-02 16:33:03 -0700

    added footer component

--- get_clicky/src/App.js
+++ get_clicky/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import characters from "./characters.json";
 import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer"
 
 import './App.css';
 import CharTile from './components/CharTile/index.js';
@@ -29,6 +30,7 @@ class App extends Component {
                   />
               )}
           </Wrapper>
+          <Footer/>
         </>
 
       );
