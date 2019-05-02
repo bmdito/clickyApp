a9562beb8d341d9b06911df1a3dcdaf77c009f64
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-05-01 23:30:32 -0700

    Nav component Added

--- get_clicky/src/App.js
+++ get_clicky/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Nav from "./components/Nav";
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +7,8 @@ function App() {
   return (
     
       <>
-        <h1>This is the Heading!</h1>
+        <Nav />
+        <h1>This is a header!</h1>
       </>
 
   );
---
+++ get_clicky/src/components/Nav/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import "./style.css"
+
+class Nav extends Component {
+
+    render(){
+
+        return (
+            <>
+                <nav className="navStyle">
+                    <h1>Let's Get Clicky!</h1>
+                </nav>
+            </>
+        )
+    }
+}
+
+export default Nav;
