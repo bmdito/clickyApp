c25270c4ba6d41732ed87b0bd621810b627f3607
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-05-07 18:04:43 -0700

    Updates

---
+++ precache-manifest.45e9ba68e047dced0e4b9b47629e477a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58e033eabee2319bd91566934ba30fe",
+    "url": "/clickyApp/index.html"
+  },
+  {
+    "revision": "fe52d92f5f9a1ef042ec",
+    "url": "/clickyApp/static/css/main.66bbcf3e.chunk.css"
+  },
+  {
+    "revision": "9508b3e097f5d4db1edd",
+    "url": "/clickyApp/static/js/2.97807068.chunk.js"
+  },
+  {
+    "revision": "fe52d92f5f9a1ef042ec",
+    "url": "/clickyApp/static/js/main.5f94d383.chunk.js"
+  },
+  {
+    "revision": "a5b244562e7d51c6386d",
+    "url": "/clickyApp/static/js/runtime~main.6a830273.js"
+  }
+]);
